19371c33f7287ca3dcb8fbc1043f00e3f7dea631
Cecília <cecilicanlyra@gmail.com>
2026-07-14 20:18:37 -0300

    add main.py and images

---
+++ main.py
@@ -0,0 +1,22 @@
+import pygame
+
+pygame.init()
+BRANCO = (255,255,255)
+
+janela = pygame.display.set_mode((700, 500))
+janela.fill(BRANCO)
+pygame.display.set_caption('Air Hockey')
+
+# button configuration
+
+
+continuar = True
+
+while continuar:
+ for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+        continuar = False
+
+    pygame.display.update()
+
+pygame.quit()
