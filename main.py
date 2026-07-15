9554e0b9ff6b773736e840a031f88d90062cff36
Cecília <cecilicanlyra@gmail.com>
2026-07-15 10:21:49 -0300

    add sounds

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ import pygame
 pygame.init()
 BRANCO = (255,255,255)
 
-janela = pygame.display.set_mode((700, 500))
+janela = pygame.display.set_mode((1024, 600))
 janela.fill(BRANCO)
 pygame.display.set_caption('Air Hockey')
 
 # button configuration
+
 
 
 continuar = True
